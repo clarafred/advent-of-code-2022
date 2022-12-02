1e6638856f4a570a83c7a12c3ec36985abbd454c
clarafred <clara.elise.fred@gmail.com>
2022-12-02 15:20:09 +0100

    refactor solution

--- day2/app/Program.cs
+++ day2/app/Program.cs
@@ -4,84 +4,61 @@
     {
         const string PATH = "./../input.txt";
 
-        var score = 0;
+        var lines = new List<string>();
 
-        var rock = "A";
-        var paper = "B";
-        var scissors = "C";
-
-        var lose = "X";
-        var draw = "Y";
-        var win = "Z";
-
-        var rockScore = 1;
-        var paperScore = 2;
-        var scissorsScore = 3;
-
-        var lostPoint = 0;
-        var drawPoint = 3;
-        var winPoint = 6;
-        
         foreach (string line in File.ReadLines(@PATH))
         {
-            var round = line.Split(" ");
-
-            var apponentsHand = round[0];
-            var end = round[1];
-
-
-            if (end == lose)
-            {
-                score += lostPoint;
-
-                if (apponentsHand == rock)
-                {
-                    score += scissorsScore;
-                }
-                else if (apponentsHand == paper)
-                {
-                    score += rockScore;
-                }
-                else if (apponentsHand == scissors)
-                {
-                    score += paperScore;
-                }
-            }
-            else if (end == draw)
-            {
-                score += drawPoint;
-
-                if (apponentsHand == rock)
-                {
-                    score += rockScore;
-                }
-                else if (apponentsHand == paper)
-                {
-                    score += paperScore;
-                }
-                else if (apponentsHand == scissors)
-                {
-                    score += scissorsScore;
-                }
-            }
-            else if (end == win)
-            {
-                score += winPoint;
-
-                if (apponentsHand == rock)
-                {
-                    score += paperScore;
-                }
-                else if (apponentsHand == paper)
-                {
-                    score += scissorsScore;
-                }
-                else if (apponentsHand == scissors)
-                {
-                    score += rockScore;
-                }
-            }
+            lines.Add(line);
         }
-        Console.WriteLine($"{score}");
+
+        var sum1 = round1(lines);
+        var sum2 = round2(lines);
+
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+    }
+
+    static int round1(List<string> lines)
+    {
+        var sum = 0;
+
+        lines.ForEach(e => 
+        {
+            var score =
+                e == "A X" ? sum += 4
+                : e == "B X" ? sum += 1
+                : e == "C X" ? sum += 7
+                : e == "A Y" ? sum += 8
+                : e == "B Y" ? sum += 5
+                : e == "C Y" ? sum += 2
+                : e == "A Z" ? sum += 3
+                : e == "B Z" ? sum += 9
+                : e == "C Z" ? sum += 6
+                : 0;
+        });
+
+        return sum;
+    }
+
+    static int round2(List<string> lines)
+    {
+        var sum = 0;
+
+        lines.ForEach(e => 
+        {
+            var score =
+                e == "A X" ? sum += 3
+                : e == "B X" ? sum += 1
+                : e == "C X" ? sum += 2
+                : e == "A Y" ? sum += 4
+                : e == "B Y" ? sum += 5
+                : e == "C Y" ? sum += 6
+                : e == "A Z" ? sum += 8
+                : e == "B Z" ? sum += 9
+                : e == "C Z" ? sum += 7
+                : 0;
+        });
+
+        return sum;
     }
 }
